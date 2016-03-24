cdde196ac83fb9a07a4e140c5e4606020ef8fcaa
gadhra <iam@stefans.computer>
2016-03-24 15:34:18 -0400

    additional design changes for nav

--- webroot/js/base.js
+++ webroot/js/base.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
+
+    // Variables
+    var $nav = $('.navbar'),
+        $body = $('body'),
+        $window = $(window),
+        $popoverLink = $('[data-popover]'),
+        navOffsetTop = $nav.offset().top,
+        $document = $(document),
+        entityMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        }
+
+
     function init() {
-        $window.on('scroll', onScroll)
-        $window.on('resize', resize)
-        $popoverLink.on('click', openPopover)
-        $document.on('click', closePopover)
-        $('a[href^="#"]').on('click', smoothScroll)
-        buildSnippets();
+        $window.on('scroll', onScroll);
+        $window.on('resize', resize);
+        $popoverLink.on('click', openPopover);
+        $document.on('click', closePopover);
+        $('a[href^="#"]').on('click', smoothScroll);
     }
 
     function smoothScroll(e) {
@@ -23,16 +40,16 @@ $(document).ready(function() {
     }
 
     function openPopover(e) {
-        e.preventDefault()
+        e.preventDefault();
         closePopover();
         var popover = $($(this).data('popover'));
-        popover.toggleClass('open')
+        popover.toggleClass('open');
         e.stopImmediatePropagation();
     }
 
     function closePopover(e) {
         if($('.popover.open').length > 0) {
-            $('.popover').removeClass('open')
+            $('.popover').removeClass('open');
         }
     }
 
@@ -43,19 +60,25 @@ $(document).ready(function() {
     });
 
     function resize() {
-        $body.removeClass('has-docked-nav')
-        navOffsetTop = $nav.offset().top
-        onScroll()
+        $body.removeClass('has-docked-nav');
+        navOffsetTop = $nav.offset().top;
+        onScroll();
     }
 
     function onScroll() {
         if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
-            $body.addClass('has-docked-nav')
+            $body.addClass('has-docked-nav');
         }
         if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-            $body.removeClass('has-docked-nav')
+            $body.removeClass('has-docked-nav');
         }
     }
+
+    function escapeHtml(string) {
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }    
 
     init();
 });
