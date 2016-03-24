ce08d4be8bbcfb609de2d266bc573e129eef4cd2
gadhra <iam@stefans.computer>
2016-03-24 16:24:49 -0400

    pull in method, do not include template until end of script

--- presenter/main.php
+++ presenter/main.php
@@ -5,19 +5,17 @@
      * @author gadhra <iam@stefans.computer>
      * @package PHP-Barebones
     */
-
+    
+    $method = Flight::get( 'method' );
     $messages = [ 'success'=>[], 'error'=> [] ];
-    import( [ 'Template' ] );
     $view = 'main.html';
-
-
-
 
 
     /**
      * View
      */
     view:
+        import( [ 'Template' ] );
         $vars = [];
         $vars['messages'] = $messages;
         $vars['presenter'] = 'main';
