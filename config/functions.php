d5fd4bf2c48dbf111a4c406541740024c30639d9
gadhra <iam@stefans.computer>
2018-04-30 19:27:58 -0400

    merging other index and config/functions files

--- config/functions.php
+++ config/functions.php
@@ -22,7 +22,58 @@
         }
         return false;
     }
+
+
+    function getDiceChain( $num = -1 ) {
+        $num = ( int ) $num;
+
+        $diceChain = [
+        'd2',
+        'd3',
+        'd4',
+        'd5',
+        'd6',
+        'd7',
+        'd8',
+        'd10',
+        'd12',
+        'd14',
+        'd16',
+        'd20',
+        'd24',
+        'd30'
+        ];
+
+        if( $num <= -1 || $num > count( $diceChain ) ) {
+            return $diceChain;
+        }
+
+        return $diceChain[$num];
+    }   
     
+    function roll20( $type = 'd20' ) {
+        $type = preg_replace( '/\s+/', '', $type );
+        preg_match( '/.*?(([0-9]*)d([0-9]*)).*/', $type, $matches );
+        if( empty( $matches ) ) {
+            return $type;
+        }
+
+        $dicestr = $matches[1];
+        $min = ( int ) $matches[2];
+        $max = ( int ) $matches[3];
+
+
+        if( empty( $min ) ) {
+            $min = 1;
+        }
+        $max = $max * $min;
+        $roll = mt_rand( $min, $max );
+        $roll_modified = str_replace( $dicestr, $roll, $type );
+        return eval( "return $roll_modified;" );
+
+
+    }
+
     /**
       * taking advantage of PHP object handling with this hack
       * and I feel a bit bad about it
--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     import([ 'flight' ]);
     
     Flight::set( 'flight.log_errors', true );
-        
+    
     Flight::map('error', function( Throwable $ex ){
         // Handle error
-        if( ENV == 'development' ) {
+        if( ENVIRONMENT == 'DEV' ) {
             if(! empty( $ex->xdebug_message ) ) {
                 echo '<pre>' . $ex->xdebug_message . '</pre>';
             } else {
@@ -19,40 +19,83 @@
         } else {
             syslog( LOG_NOTICE, $ex->getTraceAsString() );
         }
-    });
+    });   
+
 
     Flight::route( 'GET /phpinfo', function() {
-        if( ENV != 'development' ) {
+        if( ENVIRONMENT != 'DEV' ) {
             return true;
         }
         phpinfo();
     });
 
-
     Flight::route( 'GET /@name(/@id)', function( $name, $id ) {
         Flight::set( 'method', 'GET' );
-        if(! presenter( $name ) ) {
+        $data = Flight::request()->query;
+        
+        $params = [];
+        foreach( $data as $k=>$v ) {
+            $params[filter_var($k,FILTER_SANITIZE_STRING)] = filter_var($v,FILTER_SANITIZE_STRING);
+        }
+        
+        /**
+          * Override the id if passed RESTfully
+          */
+        if(! empty( $id ) ) {
+            $params['id'] = filter_var($id,FILTER_SANITIZE_STRING);
+        }
+
+
+        if(! presenter( $name, $params ) ) {
             return true;
         }
     });
     
     Flight::route( 'POST /@name', function( $name ) {
         Flight::set( 'method', 'POST' );
-        if(! presenter( $name ) ) {
+        $data = Flight::request()->data;
+        
+        $params = [];
+        foreach( $data as $k=>$v ) {
+            $params[filter_var($k,FILTER_SANITIZE_STRING)] = filter_var($v,FILTER_SANITIZE_STRING);
+        }
+        
+        if(! presenter( $name, $params ) ) {
             return true;
         }
     });
 
     Flight::route( 'PUT /@name/@id', function( $name, $id ) {
         Flight::set( 'method', 'PUT' );
-        if(! presenter( $name ) ) { 
+        $data = Flight::request()->getBody();
+        
+        $params = [];
+        foreach( $data as $k=>$v ) {
+            $params[filter_var($k,FILTER_SANITIZE_STRING)] = filter_var($v,FILTER_SANITIZE_STRING);    
+        }
+        
+        if(! presenter( $name, $params ) ) { 
             return true;
         }
     });
     
     Flight::route( 'DELETE /@name/@id', function( $name, $id ) {
         Flight::set( 'method', 'DELETE' );
-        if(! presenter( $name ) ) {
+        $data = Flight::request()->getBody();
+        
+        $params = [];
+        foreach( $data as $k=>$v ) {
+            $params[filter_var($k,FILTER_SANITIZE_STRING)] = filter_var($v,FILTER_SANITIZE_STRING);    
+        }
+
+        /**
+          * Override the id if passed RESTfully
+          */
+        if(! empty( $id ) ) {
+            $params['id'] = filter_var($id,FILTER_SANITIZE_STRING);
+        }        
+             
+        if(! presenter( $name, $params ) ) {
             return true;
         }
     });
@@ -62,4 +105,5 @@
         presenter( 'main' );
     });
     
-    Flight::start();
+
+
