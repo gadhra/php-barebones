3f9112b4090b751ccf891056744df6e8e02e496c
gadhra <iam@stefans.computer>
2018-04-30 19:29:43 -0400

    moved sessions sql definition out of lib

--- lib/Session.php
+++ lib/Session.php
@@ -1,15 +1,4 @@
 <?php
-    
-/**
- CREATE TABLE `sessions` (
-  `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `data` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-*/
-
-
     class Session implements SessionHandlerInterface {
         private $conn;
         
