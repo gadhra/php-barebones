f9980603fce2eb788728aa631b0bd9d520f85893
Stefan Antonowicz <santonowicz@umass.edu>
2018-04-30 10:27:40 -0400

    time to roll this otu for PHP7, and enforce that

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,7 @@
 <?php
+
+if( version_compare( phpversion(), '7.0', '<' ) ) {
+    die( 'Minimum version to run this application: PHP 7.0' );
+}
 require_once( dirname(__FILE__) . '/../config/load.php' );
 require_once( dirname(__FILE__). '/../index.php' );
